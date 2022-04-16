e58324368e700afcf3243e244d8806efb82936b6
ValerI Akseniko <valeri.akseniko@gmail.com>
2022-04-16 13:36:53 +0400

    commit

--- SobitsTestTask/wwwroot/js/View/Purchase/Index.js
+++ SobitsTestTask/wwwroot/js/View/Purchase/Index.js
@@ -9,25 +9,36 @@ async function Find() {
     const purchases = await FindListPurchase();
     console.log(purchases);
     for (let i = 0; i < purchases.length; i++) {
+        let table = `${purchases[i].title}
+                    <br />${purchases[i].buyer}
+                    <br />
+                        <table class="table table-hover" width="100%">
+                            <tbody id="${purchases[i].id}">
+                            </tbody>
+                        </table>
+                    <br />`;
+        $("#table-list").append(table);
         FillPurchase(purchases[i]);
     }    
 }
 
 function FillPurchase(purchase) {
 
-    $("#title").text(purchase.title);
-    $("#buyer").text(purchase.buyer);
-
     for (let i = 0; i < purchase.users.length; i++) {
         const row = CreatePurchaseRow(purchase.users[i]);
-        $("#purchase-list").append(row);
+        let id = `#${purchase.id}`;
+        $(id).append(row);
     }
 }
 function CreatePurchaseRow(userByPurchase) {
+
+    var button = `<a href=" " class="btn btn-outline-danger">Payment</a>`;
+
     const row = `<tr>
-            <td width="40%">${userByPurchase.userName}</td>
-            <td width="30%">${userByPurchase.status}</td>
-            <td width="30%">${userByPurchase.debt}</td>
+            <td width="25%">${userByPurchase.userName}</td>
+            <td width="25%">${userByPurchase.status}</td>
+            <td width="25%">${userByPurchase.debt}</td>
+            <td width="25%">${button}</td>
         </tr>`;
     return row;
 }
